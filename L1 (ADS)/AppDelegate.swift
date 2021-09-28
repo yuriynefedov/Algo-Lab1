f561726e7b1b05c83a907c8dac8aedf0794d40f1
Yuriy <yurii.nefedov@ucu.edu.ua>
2021-09-28 09:38:38 +0300

    Initial Commit

---
+++ L1 (ADS)/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  L1 (ADS)
+//
+//  Created by Yuriy Nefedov on 28.09.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ L1 (ADS)/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  L1 (ADS)
+//
+//  Created by Yuriy Nefedov on 28.09.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
